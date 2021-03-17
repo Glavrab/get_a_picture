686f949f9123e5693a116e4b11b205ff41c203ab
glavrab <g2NUYeda32200>
2021-03-18 01:33:40 +0300

    refactor: Add html_parser func to make get_picture look better

--- main.py
+++ main.py
@@ -6,29 +6,33 @@ import asyncio
 URL = 'https://www.generatormix.com/random-image-generator'
 
 
+def html_parser(data: str, object_to_find: str, **search_args) -> str:
+    """Parse html page to look for object"""
+    parsed_html_doc = BeautifulSoup(data, 'lxml')
+    searched_str = parsed_html_doc.find_all(attrs=search_args)
+    for attr in searched_str:
+        info = attr.get(object_to_find)
+        return info
+
+
 async def get_picture(num: int) -> None:
     """Get a random picture from the website"""
     session = ClientSession()
     async with session.get(URL) as response:
         html_doc = await response.text()
-        parsed_html_doc = BeautifulSoup(html_doc, 'lxml')
-        str_with_token = parsed_html_doc.find(value=True, type='hidden')
-        token = str(str_with_token)[42:82]
+        token = html_parser(data=html_doc, object_to_find='value', type='hidden')
         data = {'_token': token}
         for i in range(num):
             response = await session.post(URL, data=data)
             unparsed_response = await response.text()
-            html_doc = BeautifulSoup(unparsed_response[27:-2], 'lxml')
-            list_of_attr = html_doc.find_all(src=True)
-            for attr in list_of_attr:
-                unparsed_url_for_picture = attr.get('data-src')
-                image_name = unparsed_url_for_picture[30:-2]
-                url_for_picture = 'https://pixabay.com/get/' + image_name
-                async with session.get(url_for_picture) as response_picture:
-                    picture = await response_picture.content.read()
-                    filename = image_name
-                    with open(filename, 'xb') as new_picture:
-                        new_picture.write(picture)
+            unparsed_url_for_picture = html_parser(data=unparsed_response[27:-2], object_to_find='data-src', src=True)
+            image_name = unparsed_url_for_picture[30:-2]
+            url_for_picture = 'https://pixabay.com/get/' + image_name
+            async with session.get(url_for_picture) as response_picture:
+                picture = await response_picture.content.read()
+                filename = image_name
+                with open(filename, 'xb') as new_picture:
+                    new_picture.write(picture)
     await session.close()
 
 

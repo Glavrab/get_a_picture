4e3199fa044d47aee36c573c213f27b64542403e
glavrab <g2NUYeda32200>
2021-03-18 01:57:56 +0300

    refactor: Divide get_token and get_picture

--- main.py
+++ main.py
@@ -15,30 +15,35 @@ def html_parser(data: str, object_to_find: str, **search_args) -> str:
         return info
 
 
-async def get_picture(num: int) -> None:
-    """Get a random picture from the website"""
+async def get_token(num) -> None:
+    """Get a token from the website"""
     session = ClientSession()
-    async with session.get(URL) as response:
-        html_doc = await response.text()
-        token = html_parser(data=html_doc, object_to_find='value', type='hidden')
-        data = {'_token': token}
-        for i in range(num):
-            response = await session.post(URL, data=data)
-            unparsed_response = await response.text()
-            unparsed_url_for_picture = html_parser(data=unparsed_response[27:-2], object_to_find='data-src', src=True)
-            image_name = unparsed_url_for_picture[30:-2]
-            url_for_picture = 'https://pixabay.com/get/' + image_name
-            async with session.get(url_for_picture) as response_picture:
-                picture = await response_picture.content.read()
-                filename = image_name
-                with open(filename, 'xb') as new_picture:
-                    new_picture.write(picture)
+    response = await session.get(URL)
+    html_doc = await response.text()
+    token = html_parser(data=html_doc, object_to_find='value', type='hidden')
+    await get_picture(num=num, token=token, session=session)
+
+
+async def get_picture(num, token, session) -> None:
+    """Get a picture from the website"""
+    data = {'_token': token}
+    for i in range(num):
+        response = await session.post(URL, data=data)
+        unparsed_response = await response.text()
+        unparsed_url_for_picture = html_parser(data=unparsed_response[27:-2], object_to_find='data-src', src=True)
+        image_name = unparsed_url_for_picture[30:-2]
+        url_for_picture = 'https://pixabay.com/get/' + image_name
+        async with session.get(url_for_picture) as response_picture:
+            picture = await response_picture.content.read()
+            filename = image_name
+            with open(filename, 'xb') as new_picture:
+                new_picture.write(picture)
     await session.close()
 
 
 if __name__ == '__main__':
     pictures_to_download = int(input('How many pictures do you want?'))
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(get_picture(pictures_to_download))
+    loop.run_until_complete(get_token(pictures_to_download))
     loop.run_until_complete(asyncio.sleep(0))
     loop.close()
